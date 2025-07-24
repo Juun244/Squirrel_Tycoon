bf118ad24240e4f7ff811a88f0602cb1c8e7c330
nahosos <hjs42672882@gmail.com>
2025-07-24 22:39:44 +0900

    상하좌우 이동

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,15 +10,47 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
 
+    public TextMeshProUGUI climbHintText;
+    //private bool isNearTree = false;
+
+    private Transform nearbyTree;
+    //public bool isClimbing { get; private set; }
+
     void Start()
     {
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        //climbHintText.enabled = false;
     }
+
+    /*
+    IEnumerator ClimbTree(Vector3 startPos, float climbHeight, float duration)
+    {
+        isClimbing = true;
+
+        float elapsed = 0f;
+        Vector3 endPos = startPos + Vector3.up * climbHeight;
+
+        while (elapsed < duration)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = endPos;
+
+        // 애니메이션 상태 초기화
+        animator.SetBool("isMoving", false);
+        isClimbing = false;
+    }
+    */
 
     void Update()
     {
+        //if (isClimbing) return;
+
         // 상하 좌우 입력처리
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
@@ -24,15 +58,38 @@ public class PlayerController : MonoBehaviour
         bool isMoving = moveInput.magnitude > 0;
         animator.SetBool("isMoving", isMoving);
 
-        if (isMoving)
+        /*
+        if (isNearTree && Input.GetKeyDown(KeyCode.F))
         {
-            if(Input.GetAxisRaw("Horizontal") != 0)
-                spriteRenderer.flipX = moveInput.x > 0;
+            if (nearbyTree != null)
+            {
+                Collider2D treeCollider = nearbyTree.GetComponent<Collider2D>();
+                float treeHeight = treeCollider.bounds.size.y;
+
+                // 시작 위치 계산 (나무 아래 중앙)
+                Vector3 climbStartPos = new Vector3(
+                    nearbyTree.position.x,
+                    treeCollider.bounds.center.y - treeHeight / 2f + 0.2f,
+                    transform.position.z
+                );
+
+                transform.position = climbStartPos;
+
+                // 위로 걷는 애니메이션 재생 조건
+                animator.SetBool("isMoving", true);
+                animator.SetFloat("moveX", 0f);
+                animator.SetFloat("moveY", 1f);
+
+                StartCoroutine(ClimbTree(transform.position, 3.0f, 1.0f)); // 높이, 시간 조절 가능
+            }
         }
+        */
     }
+   
 
     void FixedUpdate()
     {
+        //if (isClimbing) return;
         Move();
     }
 
@@ -40,4 +97,28 @@ public class PlayerController : MonoBehaviour
     {
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
+    /*
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Tree"))
+        {
+            isNearTree = true;
+            climbHintText.enabled = true;
+            nearbyTree = collider.transform;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Tree"))
+        {
+            isNearTree = false;
+            climbHintText.enabled = false;
+            nearbyTree = null;
+        }
+    }
+    */
+
+
 }
